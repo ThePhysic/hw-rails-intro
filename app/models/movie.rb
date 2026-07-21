fe73675f0f1f080b9c1ddefd1c4a2c5bc4234ab1
Ridleigh Sivo <thephysic@berkeley.edu>
2026-07-21 03:43:42 +0000

    Add movie rating model methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ApplicationRecord
+  RATINGS = %w[G PG PG-13 R].freeze
+
+  def self.all_ratings
+    RATINGS
+  end
+
+  def self.with_ratings(ratings_list)
+    return all if ratings_list.blank?
+
+    normalized_ratings = ratings_list.map(&:upcase)
+    where("UPPER(rating) IN (?)", normalized_ratings)
+  end
 end
